6b640191627f2165fb5c4a2cca81e0b7c08a952f
kksk10 <122059469+kksk10@users.noreply.github.com>
2023-10-29 17:46:50 -0400

    Update azure.py

--- azure.py
+++ azure.py
@@ -6,9 +6,9 @@ import os
 # Set up your OpenAI API key
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 # Initialize Streamlit
-st.title("Azure Guide Chatbot")
+st.title("Massachusetts Legal Guide Chatbot")
 # Create a text input field for user queries
-user_input = st.text_input("Ask a question:")
+user_input = st.text_input("Ask a legal question about Massachusetts:")
 # Send the user's query to OpenAI GPT-3
 if user_input:
     response = openai.Completion.create(
